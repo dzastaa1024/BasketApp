420d39a6f984889df94514905784f36a20da0cdc
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-04-22 12:11:28 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Wrapper>
+          <div>Hello</div>
+        </Wrapper>
+      </div>
+    );
+  }
+}
+
+const Wrapper = styled.div``;
